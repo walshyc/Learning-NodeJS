359189d611db30b0acb365de39063e44fda374f8
Conor Walsh <conorwalsh0703@gmail.com>
2019-09-27 17:50:38 +0100

    added static files for css

--- 04 - Express.js/app.js
+++ 04 - Express.js/app.js
@@ -8,12 +8,13 @@ const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
