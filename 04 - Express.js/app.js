fa7c5c893fb9eedb7f2708d19d9a454dacdabd47
Conor Walsh <conorwalsh0703@gmail.com>
2019-09-27 17:06:18 +0100

    added filtering to routes

--- 04 - Express.js/app.js
+++ 04 - Express.js/app.js
@@ -8,7 +8,11 @@ const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
