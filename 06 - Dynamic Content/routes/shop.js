886db2f030c1721f5e45776165c70115e5f7e59e
Conor Walsh <conorwalsh0703@gmail.com>
2019-09-28 12:02:52 +0100

    installed templating engines

--- 06 - Dynamic Content/routes/shop.js
+++ 06 - Dynamic Content/routes/shop.js
@@ -2,10 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-
+const adminData = require('./admin.js');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.products)
     res.sendFile(path.join(rootDir ,'views','shop.html'));
 });
 
