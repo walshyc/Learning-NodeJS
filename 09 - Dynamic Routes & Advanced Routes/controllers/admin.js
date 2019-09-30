e0c269afc969e1086117eb75d732c58c7bb1f2b5
Conor Walsh <conorwalsh0703@gmail.com>
2019-09-30 16:45:10 +0100

    added placeholders to product edit form

--- 09 - Dynamic Routes & Advanced Routes/controllers/admin.js
+++ 09 - Dynamic Routes & Advanced Routes/controllers/admin.js
@@ -4,7 +4,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product'
+        path: '/admin/add-product',
+        editing: false
     });
 };
 
@@ -21,14 +22,24 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
 
-    if(!editMode) {
-       return res.redirect('/');
+    if (!editMode) {
+        return res.redirect('/');
     }
-    res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode
+
+    const prodID = req.params.productID;
+    Product.findbyID(prodID, product => {
+        if(!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
     });
+
+
 };
 
 exports.getProducts = (req, res, next) => {
