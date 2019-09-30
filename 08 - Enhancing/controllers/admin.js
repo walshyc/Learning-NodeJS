7c10fca4837b7fd63da3e1fe15296b62849ad8db
Conor Walsh <conorwalsh0703@gmail.com>
2019-09-30 14:01:57 +0100

    upadted product model and views

--- 08 - Enhancing/controllers/admin.js
+++ 08 - Enhancing/controllers/admin.js
@@ -9,7 +9,11 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageURL = req.body.imageURL;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageURL, description, price);
     product.save();
     res.redirect('/');
 };
--- 08 - Enhancing/models/product.js
+++ 08 - Enhancing/models/product.js
@@ -16,8 +16,11 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageURL, description, price) {
+        this.title = title;
+        this.imageURL = imageURL;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
