c8101056722ad864364d46c2f83cfd6d806ead41
Conor Walsh <conorwalsh0703@gmail.com>
2019-09-30 13:04:37 +0100

    created models

--- 07 - Model View Controller (MVC)/controllers/products.js
+++ 07 - Model View Controller (MVC)/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -8,16 +8,18 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({
-        title: req.body.title
-    });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/'
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/'
+        });
     });
+   
 };
