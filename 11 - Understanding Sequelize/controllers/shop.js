cdac8190bf93909e1513cdd7bc25b1aa8e553586
Conor Walsh <conorwalsh0703@gmail.com>
2019-10-04 16:17:46 +0100

    added find by ID

--- 11 - Understanding Sequelize/controllers/shop.js
+++ 11 - Understanding Sequelize/controllers/shop.js
@@ -16,17 +16,26 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodID = req.params.productID;
-    Product.findbyID(prodID)
-        .then(([product]) => {
-            res.render('shop/product-detail', {
-                product: product[0],
-                pageTitle: `${product.title} Information`,
-                path: '/products'
-            });
-        })
-        .catch(err => console.log(err));
-
-
+    // Product.findAll({where: {
+    //     id: prodID
+    // }})
+    // .then(products => {
+    //     res.render('shop/product-detail', {
+    //         product: products[0],
+    //         pageTitle: `${products[0].title} Information`,
+    //         path: '/products'
+    //     });
+    // })
+    // .catch(err => console.log(err));
+     Product.findByPk(prodID)
+         .then(product => {
+             res.render('shop/product-detail', {
+                 product: product,
+                 pageTitle: `${product.title} Information`,
+                 path: '/products'
+             });
+         })
+         .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
