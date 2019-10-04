c7a50e8cc5716092b524b5712a0017d8fa9a0f12
Conor Walsh <conorwalsh0703@gmail.com>
2019-10-04 15:23:44 +0100

    defined product model

---
+++ 11 - Understanding Sequelize/controllers/admin.js
@@ -0,0 +1,76 @@
+const Product = require('../models/product');
+
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const imageURL = req.body.imageURL;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(null, title, imageURL, description, price);
+    product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+
+exports.postEditProduct = (req, res, next) => {
+    const prodID = req.body.productID;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedDescription = req.body.description;
+    const updatedURL = req.body.imageURL;
+    const updatedProduct = new Product(prodID, updatedTitle, updatedURL, updatedDescription, updatedPrice);
+    updatedProduct.save();
+
+    res.redirect('/admin/products');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    const prodID = req.params.productID;
+    Product.findbyID(prodID, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    });
+
+
+};
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
+        });
+    });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodID = req.body.productID;
+    Product.deleteByID(prodID);
+    res.redirect('/admin/products');
+};
---
+++ 11 - Understanding Sequelize/controllers/shop.js
@@ -0,0 +1,101 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/product-list', {
+                prods: rows,
+                pageTitle: 'All Products',
+                path: '/products'
+            });
+        })
+        .catch(err => console.log(err));
+
+};
+
+exports.getProduct = (req, res, next) => {
+    const prodID = req.params.productID;
+    Product.findbyID(prodID)
+        .then(([product]) => {
+            res.render('shop/product-detail', {
+                product: product[0],
+                pageTitle: `${product.title} Information`,
+                path: '/products'
+            });
+        })
+        .catch(err => console.log(err));    
+    
+
+};
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/index', {
+                prods: rows,
+                pageTitle: 'Shop',
+                path: '/'
+            });
+        })
+        .catch(err => console.log(err));
+
+};
+
+exports.postCart = (req, res, next) => {
+    const prodID = req.body.productID;
+    Product.findbyID(prodID, product => {
+        Cart.addProduct(prodID, product.price);
+    });
+    res.redirect('/cart');
+};
+
+
+exports.getCart = (req, res, next) => {
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    cartProducts.push({
+                        productData: product,
+                        qty: cartProductData.qty
+                    });
+                }
+            }
+
+            const cartTotalPrice = Cart.totalPrice;
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts,
+                cartTotal: cartTotalPrice
+            });
+        });
+    });
+
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const prodID = req.body.productID;
+    Product.findbyID(prodID, product => {
+        Cart.deleteProduct(prodID, product.price);
+        res.redirect('/cart');
+    });
+
+};
+
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout'
+    });
+};
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    });
+};
