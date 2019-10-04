2906767716d6055c61e080aada342b0bf3ec49d5
Conor Walsh <conorwalsh0703@gmail.com>
2019-10-04 16:05:36 +0100

    added find all products from database

--- 11 - Understanding Sequelize/controllers/shop.js
+++ 11 - Understanding Sequelize/controllers/shop.js
@@ -2,10 +2,10 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
+    Product.findAll()
+        .then(products => {
             res.render('shop/product-list', {
-                prods: rows,
+                prods: products,
                 pageTitle: 'All Products',
                 path: '/products'
             });
@@ -24,22 +24,21 @@ exports.getProduct = (req, res, next) => {
                 path: '/products'
             });
         })
-        .catch(err => console.log(err));    
-    
+        .catch(err => console.log(err));
+
 
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
+    Product.findAll()
+        .then(products => {
             res.render('shop/index', {
-                prods: rows,
+                prods: products,
                 pageTitle: 'Shop',
                 path: '/'
             });
         })
         .catch(err => console.log(err));
-
 };
 
 exports.postCart = (req, res, next) => {
