c54478283f49db580ffc399cbf64be67640dea8b
Conor Walsh <conorwalsh0703@gmail.com>
2019-10-18 16:48:47 +0100

    created user model

--- 11 - Understanding Sequelize/app.js
+++ 11 - Understanding Sequelize/app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
-
+const Product = require('./models/product');
+const User = require('./models/user');
 const app = express();
 
 
@@ -26,11 +27,18 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    // console.log(result);
-    app.listen(3000);
-})
-.catch(err => {
-    console.log(err);
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
 });
+User.hasMany(Product);
 
+sequelize
+    .sync({force: true})
+    .then(result => {
+        // console.log(result);
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
