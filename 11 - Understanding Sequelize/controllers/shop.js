e05766878f6354af7f67c64c35ebd23367fe1cc1
Conor Walsh <conorwalsh0703@gmail.com>
2019-10-21 13:22:16 +0100

    added add to order

--- 11 - Understanding Sequelize/controllers/shop.js
+++ 11 - Understanding Sequelize/controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.findAll()
@@ -146,4 +145,29 @@ exports.getOrders = (req, res, next) => {
         path: '/orders',
         pageTitle: 'Your Orders'
     });
+};
+
+exports.postOrder = (req, res, next) => {
+    req.user
+        .getCart()
+        .then(cart => {
+            return cart.getProducts();
+        })
+        .then(products => {
+            return req.user
+                .createOrder()
+                .then(order => {
+                   return order.addProducts(products.map(product => {
+                        product.orderItem = {
+                            quantity: product.cartItem.quantity
+                        };
+                        return product;
+                    }));
+                })
+                .catch(err => console.log(err));
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
 };
--- 11 - Understanding Sequelize/routes/shop.js
+++ 11 - Understanding Sequelize/routes/shop.js
@@ -21,4 +21,6 @@ router.get('/checkout', shopController.getCheckout);
 
 router.get('/orders', shopController.getOrders);
 
+router.post('/create-order', shopController.postOrder);
+
 module.exports = router;
