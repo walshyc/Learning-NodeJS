5ab6d4bcf23ab6757aa73a3772006a313354374c
Conor Walsh <conorwalsh0703@gmail.com>
2019-10-22 16:22:51 +0100

    updated to show a single product

--- 13 - Working with Mongoose/controllers/shop.js
+++ 13 - Working with Mongoose/controllers/shop.js
@@ -18,7 +18,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
     const prodID = req.params.productID;
     Product
-        .findByID(prodID)
+        .findById(prodID)
         .then(product => {
             res.render('shop/product-detail', {
                 product: product,
