df5a84eb50dd3b28fa3849bd219ae3bcdbce9c4e
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-01 15:12:19 +0000

    stored sessions in mongoDB

--- 14 - Sessions + Cookies/app.js
+++ 14 - Sessions + Cookies/app.js
@@ -3,11 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
+
+const MONGODB_URI = 'mongodb+srv://conorw:zP5o2576NSCVhxvt@cluster0-srq59.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views'); //sets where the HTMl templates are contained, not required if using views folder
@@ -32,7 +40,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'A%D*G-KaPdSgVkYp2s5v8y/B?E(H+MbQ',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }));
 
 app.use('/admin', adminRoutes);
@@ -43,7 +52,7 @@ app.use(errorController.get404);
 
 
 mongoose
-    .connect('mongodb+srv://conorw:zP5o2576NSCVhxvt@cluster0-srq59.mongodb.net/shop?retryWrites=true&w=majority')
+    .connect(MONGODB_URI)
     .then(result => {
         User.findOne().then(user => {
             if (!user) {
