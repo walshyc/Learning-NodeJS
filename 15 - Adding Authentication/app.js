09015eb59c5807c78dc23048547d4e739d40b526
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-04 22:20:54 +0000

    added csrf token protection

--- 15 - Adding Authentication/app.js
+++ 15 - Adding Authentication/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,10 +17,10 @@ const store = new MongoDBStore({
     collection: 'sessions'
 });
 
+const csrfProtection = csrf();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views'); //sets where the HTMl templates are contained, not required if using views folder
-
-
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
@@ -37,6 +37,8 @@ app.use(session({
     store: store
 }));
 
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
@@ -48,6 +50,13 @@ app.use((req, res, next) => {
             next();
         })
         .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+
+    next();
 });
 
 app.use('/admin', adminRoutes);
