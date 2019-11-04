71a247d1025c2da7ee7064d0992cc74ef2013c0b
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-04 23:10:34 +0000

    started module

---
+++ 16 - Sending Emails/controllers/auth.js
@@ -0,0 +1,118 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: message
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User
+        .findOne({
+            email: email
+        })
+        .then(user => {
+            if (!user) {
+                req.flash('error', 'Invalid email');
+                return res.redirect('/login');
+            }
+
+            bcrypt
+                .compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    req.flash('error', 'Invalid password');
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        req.flash('logout', 'You have been logged out');
+        res.redirect('/');
+    });
+
+};
+
+exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        errorMessage: message
+    });
+};
+
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    User
+        .findOne({
+            email: email
+        })
+        .then(userDoc => {
+            if (userDoc) {
+                req.flash('error', 'Account already exists for this email.');
+                return res.redirect('/signup');
+            }
+            return bcrypt
+                .hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: {
+                            items: []
+                        }
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    res.redirect('/login');
+                });
+        })
+        .catch(err => console.log(err));
+};
+
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+
+        res.redirect('/');
+    });
+
+};
---
+++ 16 - Sending Emails/models/order.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    products: [{
+        product: {
+            type: Object,
+            require: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        
+    }],
+    user: {
+        email: {
+            type: String,
+            required: true
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        }
+    },
+    total: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
