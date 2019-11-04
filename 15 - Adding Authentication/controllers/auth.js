3883db63d19a50f024834d8cd6a27f0e3cdcae75
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-04 19:56:35 +0000

    created login

--- 15 - Adding Authentication/controllers/auth.js
+++ 15 - Adding Authentication/controllers/auth.js
@@ -15,16 +15,35 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User
-        .findById('5daf237607f3831f1484ae36')
-        .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
-                console.log(err);
-                res.redirect('/');
-            });
+    const email = req.body.email;
+    const password = req.body.password;
 
+    User
+        .findOne({
+            email: email
+        })
+        .then(user => {
+            if (!user) {
+                return res.redirect('/login');
+            }
+
+            bcrypt
+                .compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.redirect('login');
+                });
         })
         .catch(err => console.log(err));
 };
