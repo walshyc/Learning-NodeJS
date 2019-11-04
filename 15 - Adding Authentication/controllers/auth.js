dd546dabc960f01335d9f2b161ab241f17dd64ba
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-04 18:16:34 +0000

    started module

---
+++ 15 - Adding Authentication/controllers/auth.js
@@ -0,0 +1,37 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    // const isLoggedIn = req
+    //     .get('Cookie')
+    //     .split(';')[1]
+    //     .trim()
+    //     .split('=')[1];
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    User
+        .findById('5daf237607f3831f1484ae36')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+            });
+            
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
+
+};
