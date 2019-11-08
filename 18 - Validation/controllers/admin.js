44fa68d342a4ccec0dbdd560cf17faed99bdbd06
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-08 17:18:41 +0000

    added validation to edit product page

--- 18 - Validation/controllers/admin.js
+++ 18 - Validation/controllers/admin.js
@@ -61,8 +61,26 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
     const updatedUrl = req.body.imageURL;
+    const errors = validationResult(req);
 
-
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: true,
+            hasError: true,
+            product: {
+                title: updatedTitle,
+                imageUrl: updatedUrl,
+                price: updatedPrice,
+                description: updatedDescription,
+                _id: prodID                
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
 
     Product
         .findById(prodID)
@@ -102,7 +120,8 @@ exports.getEditProduct = (req, res, next) => {
                 editing: editMode,
                 product: product,
                 hasError: false,
-                errorMessage: null
+                errorMessage: null,
+                validationErrors: []
             });
         })
         .catch(err => console.log(err));
