1670d007bc546f91ed1ba134301902df88a8544d
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-08 17:04:32 +0000

    added validation for product adding

--- 18 - Validation/controllers/admin.js
+++ 18 - Validation/controllers/admin.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Product = require('../models/product');
+const {
+    validationResult
+} = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false
+        editing: false,
+        hasError: false,
+        errorMessage: null
     });
 };
 
@@ -14,6 +19,26 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageURL;
     const price = req.body.price;
     const description = req.body.description;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description                
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
     const product = new Product({
         title: title,
         price: price,
@@ -36,6 +61,8 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
     const updatedUrl = req.body.imageURL;
+
+
 
     Product
         .findById(prodID)
@@ -73,7 +100,9 @@ exports.getEditProduct = (req, res, next) => {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
                 editing: editMode,
-                product: product
+                product: product,
+                hasError: false,
+                errorMessage: null
             });
         })
         .catch(err => console.log(err));
