20459428afd91943dd719c719e50170d59d6dce8
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-08 12:50:48 +0000

    updated signup form

--- 18 - Validation/controllers/auth.js
+++ 18 - Validation/controllers/auth.js
@@ -32,6 +32,18 @@ exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res
+            .status(422) // error status code
+            .render('auth/login', {
+                path: '/login',
+                pageTitle: 'Login',
+                errorMessage: errors.array()[0].msg
+            });
+    }
+
     User
         .findOne({
             email: email
@@ -83,7 +95,12 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
     });
 };
 
@@ -98,7 +115,12 @@ exports.postSignup = (req, res, next) => {
             .render('auth/signup', {
                 path: '/signup',
                 pageTitle: 'Signup',
-                errorMessage: errors.array()[0].msg
+                errorMessage: errors.array()[0].msg,
+                oldInput: {
+                    email: email,
+                    password: password,
+                    confirmPassword: req.body.confirmPassword
+                }
             });
     }
 
