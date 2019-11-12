e838660c095c9f14a612ca06083dd7bfd756c664
Conor Walsh <conorwalsh0703@gmail.com>
2019-11-12 23:03:50 +0000

    added dynamic pagination

--- 21 - Pagination/controllers/shop.js
+++ 21 - Pagination/controllers/shop.js
@@ -5,6 +5,7 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const PDFDocument = require('pdfkit');
 
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
     Product
@@ -20,7 +21,7 @@ exports.getProducts = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 
 };
 
@@ -39,24 +40,41 @@ exports.getProduct = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 };
 
 exports.getIndex = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+
     Product
         .find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product
+                .find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then(products => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/'
+                path: '/',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 };
 
 exports.postCart = (req, res, next) => {
@@ -72,7 +90,7 @@ exports.postCart = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 };
 
 
@@ -94,7 +112,7 @@ exports.getCart = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 
 };
 
@@ -109,7 +127,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 };
 
 exports.getOrders = (req, res, next) => {
@@ -127,7 +145,7 @@ exports.getOrders = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 };
 
 exports.postOrder = (req, res, next) => {
@@ -165,7 +183,7 @@ exports.postOrder = (req, res, next) => {
             const error = new Error(err);
             error.httpStatusCode = 500;
             return next(error);
-          });
+        });
 };
 
 exports.getInvoice = (req, res, next) => {
@@ -195,7 +213,7 @@ exports.getInvoice = (req, res, next) => {
                 pdfDoc.fontSize(14).text(prod.product.title + " - " + prod.quantity + " x " + "€" + prod.product.price);
             });
             pdfDoc.fontSize(26).text('------------------------');
-            pdfDoc.fontSize(22).text('Order Total:' + '€'+ order.total);
+            pdfDoc.fontSize(22).text('Order Total:' + '€' + order.total);
 
 
             pdfDoc.end();
